1fb6f4fa39d61b8e83bbd0a4dd931416c9ff7bde
Daniel235-web <samueledaniel235@gmail.com>
2023-11-06 19:57:21 +0000

    landing page

---
+++ app/components/HomeBanner.tsx
@@ -0,0 +1,15 @@
+const HomeBanner = () => {
+    return ( 
+    <div className="relative bg-gradient-to-r
+     from-sky-500 to-sky-700 mb-8">
+        <div>
+            <div></div>
+            <div></div>
+
+        </div> 
+        
+    </div>
+     );
+}
+ 
+export default HomeBanner;
