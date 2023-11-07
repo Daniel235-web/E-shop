5636047a572a86fd53b3b82a7c28a1ce4a548b3f
Daniel235-web <samueledaniel235@gmail.com>
2023-11-07 01:01:32 +0000

    landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import { products } from "@/utils/product";
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+import { truncateText } from "@/utils/truncateText";
+
 export default function Home() {
   return (
-    <div>E-shop</div>
-    )
+    <div className="p-8 ">
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+        <div>
+          {products.map((product: any) =>{
+            return <div>
+              {truncateText (product.name)}
+            </div>
+          })}
+        </div>
+      </Container>
+    </div>
+  );
+  
   }
